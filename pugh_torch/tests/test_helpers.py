41f0284b73a12c84a11cb926244020b47f71628e
brianpugh <bnp117@gmail.com>
2020-09-20 19:47:19 -0700

    test_add_text_under_img_grayscale

--- pugh_torch/tests/test_helpers.py
+++ pugh_torch/tests/test_helpers.py
@@ -20,4 +20,9 @@ def test_add_text_under_img_tiny(chelsea, assert_img_equal):
     annotated = helpers.add_text_under_img(chelsea, "this string is much longer than the image is wide.")
     assert_img_equal(annotated)
 
+def test_add_text_under_img_grayscale(chelsea, assert_img_equal):
+    chelsea = cv2.cvtColor(chelsea, cv2.COLOR_RGB2GRAY)
+    assert chelsea.ndim == 2
+    annotated = helpers.add_text_under_img(chelsea, "this is a grayscale image.")
+    assert_img_equal(annotated)
 
