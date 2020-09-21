05f0a295f712ef934ecddf5386fc25e079a37a85
brianpugh <bnp117@gmail.com>
2020-09-20 19:24:05 -0700

    add_text_under_img initial commit

--- pugh_torch/helpers.py
+++ pugh_torch/helpers.py
@@ -1,5 +1,7 @@
 import re
 import hashlib
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
 
 
 def camel_to_snake(s):
@@ -51,3 +53,75 @@ def compare_hash(expected, actual):
         return
 
     raise HashMismatchError(f"{actual} doesn't match expected hash {expected}")
+
+
+def add_text_under_img(img, text, font_size=None, min_font_size=10, font="DejaVuSansMono.ttf", bg="white", fg="black"):
+    """ Rasterize and add text under an image.
+
+    Based on:
+        https://stackoverflow.com/a/4902713/13376237
+
+    Parameters
+    ----------
+    img : numpy.ndarray or PIL.Image.Image
+        (H,W,3) Image
+    text : str
+        Text to display under the image
+    min_font_size : int
+        Minimum font size to render. If the resulting text is wider than the
+        passed in img, then the img will be resized.
+        Ignored if ``font_size`` is provided.
+    font_size : int
+        If provided, uses this font size and doesn't auto-search for a font size.
+    bg: str or tuple
+        Background color of annotation. Defaults to white.
+    fg: str or tuple
+        Text color. Defaults to black.
+
+    Returns
+    -------
+    numpy.ndarray
+        Resulting annotated image. The image may be rescaled depending on
+        ``min_font_size``.
+    """
+
+    if isinstance(img, np.ndarray):
+        img = Image.fromarray(img)
+
+    if font_size is None:
+        # Figure out biggest font that will fit.
+        font_size = min_font_size
+        font_obj = ImageFont.truetype(font, font_size)
+        jump_size = 75
+        while True:
+            if font_obj.getsize(text)[0] < img.size[0]:
+                font_size += jump_size
+            else:
+                jump_size = int(jump_size / 2)
+                font_size -= jump_size
+            font_obj = ImageFont.truetype(font, font_size)
+            if jump_size <= 1:
+                break
+
+        if font_size < min_font_size:
+            font_size = min_font_size
+            font_obj = ImageFont.truetype(font, font_size)
+    else:
+        font_obj = ImageFont.truetype(font, font_size)
+
+    # Check and resize img if the font doesn't fit
+    font_dim = font_obj.getsize(text)
+    if font_dim[0] > img.size[0]:
+        new_w = font_dim[0]
+        new_h = int(new_w / img.size[0] * img.size[1]) 
+        img = img.resize((new_w, new_h))
+
+    output = Image.new('RGB', (img.size[0], img.size[1] + font_dim[1]), color=bg)
+    output.paste(img)
+
+    draw = ImageDraw.Draw(output)
+    draw.text((0, img.size[1]), text, font=font_obj, fill=fg)
+
+    # Convert to a numpy object
+    return np.array(output)
+
--- pugh_torch/tests/test_helpers.py
+++ pugh_torch/tests/test_helpers.py
@@ -6,3 +6,8 @@ from pugh_torch import helpers
 def test_camel_to_snake():
     assert "some_fake_class" == helpers.camel_to_snake("SomeFakeClass")
     assert "ade20k" == helpers.camel_to_snake("ADE20k")
+
+def test_add_text_under_img(chelsea, assert_img_equal):
+    annotated = helpers.add_text_under_img(chelsea, "hello world")
+    assert_img_equal(annotated)
+
