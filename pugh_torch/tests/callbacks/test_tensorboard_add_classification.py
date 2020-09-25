5f5b108ce304e78eacf4a411e0f62c1ac14fe6b6
brianpugh <bnp117@gmail.com>
2020-09-24 19:35:49 -0700

    Added more tests to the TensorboardAddClassification

--- pugh_torch/tests/callbacks/test_tensorboard_add_classification.py
+++ pugh_torch/tests/callbacks/test_tensorboard_add_classification.py
@@ -17,6 +17,10 @@ def fake_pl_module(mocker):
     return pl_module
 
 @pytest.fixture
+def classes():
+    return ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+@pytest.fixture
 def fake_trainer(mocker):
     trainer = mocker.MagicMock()
     trainer.logger = mocker.MagicMock(TensorBoardLogger)
@@ -44,10 +48,31 @@ def test_callback_action(mocker, tmp_path, fake_trainer, fake_batch, fake_pl_mod
             'Truth: 2 (N/A)\nPred: 6 (N/A)',
             ]
 
-def test_callback_skip(mocker, tmp_path, fake_batch):
-    # TODO
-    pass
+def test_callback_skip(mocker, tmp_path, fake_batch, fake_trainer):
+    callback = TensorBoardAddClassification()
+    batch_idx = callback.logging_batch_interval - 2
+    dataloader_idx = 0
 
-def test_callback_classes():
-    # TODO
-    pass
+    callback.on_train_batch_end(fake_trainer, fake_pl_module, fake_batch, batch_idx, dataloader_idx)
+    fake_trainer.logger.experiment.add_rgb.assert_not_called()
+
+def test_callback_classes(mocker, tmp_path, fake_trainer, fake_batch, fake_pl_module, classes):
+    callback = TensorBoardAddClassification(classes=classes)
+
+    batch_idx = callback.logging_batch_interval - 1
+    dataloader_idx = 0
+
+    callback.on_train_batch_end(fake_trainer, fake_pl_module, fake_batch, batch_idx, dataloader_idx)
+
+    fake_trainer.logger.experiment.add_rgb.assert_called_once()
+    args, kwargs = fake_trainer.logger.experiment.add_rgb.call_args_list[0]
+    assert args[0] == "train/output"
+    assert (args[1] == fake_batch[0]).all()
+    assert kwargs["global_step"] == 5555
+    assert kwargs["labels"] == [
+            f'Truth: 4 ({classes[4]})\nPred: 7 ({classes[7]})',
+            f'Truth: 3 ({classes[3]})\nPred: 7 ({classes[7]})',
+            f'Truth: 1 ({classes[1]})\nPred: 6 ({classes[6]})',
+            f'Truth: 0 ({classes[0]})\nPred: 1 ({classes[1]})',
+            f'Truth: 2 ({classes[2]})\nPred: 6 ({classes[6]})',
+            ]
