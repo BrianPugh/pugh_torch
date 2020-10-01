24679c06c40d9cd7c9c84f853382417357ae1386
brianpugh <bnp117@gmail.com>
2020-09-30 17:07:16 -0700

    Add smooth labels to hetero_cross_entropy. Fixed a bug in how the super class portion of hetero_cross_entropy was computed

--- pugh_torch/losses/label_smoothing.py
+++ pugh_torch/losses/label_smoothing.py
@@ -14,6 +14,7 @@ def label_smoothing(
     reduction="mean",
     ignore_index=-100,
     alpha=0.1,
+    num_classes=None,
 ):
     """Computes the smoothed labels and applies cross-entropy loss.
 
@@ -33,7 +34,11 @@ def label_smoothing(
         If this value is 0, then this is the same as vanilla cross-entropy.
         If this value is 1, then you get a uniform distribution, which
         wouldn't be very useful.
-
+        Defaults to 0.1
+    num_classes : int
+        [optional] Used to compute smoothing amount.
+        if not provided, defaults to the number of classes inferred by the ``input``
+        shape.
     """
 
     assert 0.0 <= alpha < 1.0
@@ -42,7 +47,8 @@ def label_smoothing(
         raise NotImplementedError
 
     reducer = reduction_str(reduction)
-    num_classes = input.shape[1]
+    if num_classes is None:
+        num_classes = input.shape[1]
     uniform = torch.full_like(input, alpha / num_classes)
     one_hot = F.one_hot(target, num_classes=num_classes)
     one_hot = move_dim(one_hot, -1, 1)
