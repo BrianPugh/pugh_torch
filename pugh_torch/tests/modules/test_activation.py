1194813f815ec0f884d91f60ef3a7bc6aad58272
brianpugh <bnp117@gmail.com>
2020-10-11 14:14:36 -0700

    unit tests for sine

--- pugh_torch/tests/modules/test_activation.py
+++ pugh_torch/tests/modules/test_activation.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 from torch import nn
 import pugh_torch as pt
+import numpy as np
 
 
 class SimpleInheritedReLU(nn.ReLU, pt.modules.ActivationModule):
@@ -46,3 +47,23 @@ def test_activation_factory_function_init_first_layer():
     fc = nn.Linear(2, 2)
     fn = pt.modules.Activation("simpleinheritedreluwithinit", fc, first=True)
     assert (fc.weight == 2).all()
+
+def test_sine_fn():
+    input = np.arange(0, 5, 100)
+    expected = np.sin(input)
+    input = torch.Tensor(input)
+    fn = pt.modules.Activation("sine")
+    actual = fn(input).numpy()
+    assert np.isclose(expected, actual).all()
+
+def test_sine_first_layer():
+    input = np.arange(0, 5, 100)
+    expected = np.sin(input)
+    fc = nn.Linear(2, 2)
+    fn = pt.modules.Activation("sine", fc, first=True)
+
+def test_sine_layer():
+    input = np.arange(0, 5, 100)
+    expected = np.sin(input)
+    fc = nn.Linear(2, 2)
+    fn = pt.modules.Activation("sine", fc)
