5b361937aade3a2a18debbcfb77e693d13eecdfb
brianpugh <bnp117@gmail.com>
2020-10-15 21:32:30 -0700

    Add adabelief optimizer

--- pugh_torch/optimizers/__init__.py
+++ pugh_torch/optimizers/__init__.py
@@ -1,16 +1,20 @@
 import torch
+from adabelief_pytorch import AdaBelief
+from ranger_adabelief import RangerAdaBelief
 
 optimizer_lookup = {
+    "adabelief": AdaBelief,
     "adadelta": torch.optim.Adadelta,
     "adagrad": torch.optim.Adagrad,
     "adam": torch.optim.Adam,
     "adamw": torch.optim.AdamW,
-    "sparseadam": torch.optim.SparseAdam,
     "asgd": torch.optim.ASGD,
     "lbfgs": torch.optim.LBFGS,
+    "rangeradabelief": RangerAdaBelief,
     "rmsprop": torch.optim.RMSprop,
     "rprop": torch.optim.Rprop,
     "sgd": torch.optim.SGD,
+    "sparseadam": torch.optim.SparseAdam,
 }
 
 from .base import Optimizer
