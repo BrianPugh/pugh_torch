e27a1f7e38500ccb5ce63cb13c3297436ab46a1b
brianpugh <bnp117@gmail.com>
2020-10-19 09:38:46 -0700

    proper assertion for basic gdrive load_state_dict_from_url

--- pugh_torch/tests/models/test_io.py
+++ pugh_torch/tests/models/test_io.py
@@ -4,10 +4,45 @@ import gdown
 from pathlib import Path
 import torch
 from torch import nn
+import shutil
 
 GDRIVE_TEST_FILE_URL = (
     "https://drive.google.com/file/d/1T8TuY-8w8XVsbOI15mJZQ9OKexBqUwPZ/view?usp=sharing"
 )
+
+
+@pytest.fixture
+def model():
+    return nn.Linear(2, 4)
+
+
+@pytest.fixture
+def model_path(tmp_path):
+    return tmp_path / "foo.pth"
+
+
+@pytest.fixture
+def tmp_download_path(tmp_path):
+    return tmp_path / "download_cache.pth"
+
+
+@pytest.fixture
+def mock_gdrive_download(mocker, model, model_path, tmp_download_path):
+
+    mock = mocker.patch("pugh_torch.models.io.gdrive_download")
+
+    state_dict = model.state_dict()
+
+    # Save it to a file that we'll copy over
+    torch.save(state_dict, tmp_download_path)
+
+    def side_effect(url, local):
+        shutil.copy(tmp_download_path, local)
+        return local
+
+    mock.side_effect = side_effect
+
+    return mock
 
 
 def assert_state_dict_equal(expected, actual):
@@ -15,23 +50,24 @@ def assert_state_dict_equal(expected, actual):
         assert (expected[key] == actual[key]).all()
 
 
-def test_load_state_dict_from_url(mocker, tmp_path):
+def test_load_state_dict_from_gdrive_url(
+    mocker, model, model_path, mock_gdrive_download
+):
     """Tests normal downloading and loading a checkpoint from a url"""
 
-    state_dict_path = tmp_path / "foo.pth"
-
     # Create a state_dict
-    network = nn.Linear(2, 4)
-    expected_state_dict = network.state_dict()
+    expected_state_dict = model.state_dict()
 
-    # Save it to a file
-    torch.save(expected_state_dict, state_dict_path)
+    url = "https://drive.google.com/fake_url"
 
-    mock_gdrive_download = mocker.patch("pugh_torch.models.io.gdrive_download")
-    mock_gdrive_download.return_value = state_dict_path
+    actual_state_dict = pt.models.io.load_state_dict_from_url(url, model_path)
 
-    actual_state_dict = pt.models.io.load_state_dict_from_url(
-        "https://drive.google.com/fake_url", state_dict_path
-    )
+    mock_gdrive_download.assert_called_once_with(url, model_path)
 
     assert_state_dict_equal(expected_state_dict, actual_state_dict)
+
+
+def test_load_state_dict_from_gdrive_url_cached(
+    mocker, model, model_path, mock_gdrive_download
+):
+    pass
